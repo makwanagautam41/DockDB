946baa1b09230aa31357c81c39cf6874725f2824
Gautam Makwana <makwanagautam411@gmail.com>
2026-02-22 13:37:51 +0530

    reconfig the docker files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: true,
-    port: 5173,
+    port: 5170,
     hmr: {
       overlay: false,
     },
